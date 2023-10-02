3d3a4b249a7f9d2706e53236b16abae9f07aa0e1
MatthewMyWu <matthew6086888@hotmail.com>
2023-10-02 13:22:28 -0600

    WIP implementation for calculating cumulative stats

--- DataCleaning/cumulativeStats/cumulative_data_builder.py
+++ DataCleaning/cumulativeStats/cumulative_data_builder.py
@@ -1,5 +1,8 @@
 # Class to build up "cumulative stats" for each team
 
+from tokenize import Number
+
+
 class Cumulative_Stats_Builder:
     def __init__(self) -> Cumulative_Stats_Builder:
         self.team_stats = {}
@@ -10,6 +13,8 @@ class Cumulative_Stats_Builder:
                 n_games: int,
                 weighted_count: number,
                 cumulativeStats: {
+                    gold_differential_at_14: number,
+                    gold_differential_per_minute: number,
                     ...
                 }
             },
@@ -17,8 +22,32 @@ class Cumulative_Stats_Builder:
         }
         """
         
-    def addGamePlayed(game_info: dict, stats_info: dict) -> tuple(dict, dict):
-        pass
+    def addGamePlayed(self, game_info: dict, stats_info: dict) -> tuple(dict, dict):
+        team1_id, team2_id = self._getTeamIdFromGameInfo(game_info)
+        self._updateTeamCumulativeStats(team_id=team1_id, game_info=game_info, stats_info=stats_info)
+        self._updateTeamCumulativeStats(team_id=team2_id, game_info=game_info, stats_info=stats_info)
+        return (self.getCumulativeStatsForTeam(team1_id), self.getCumulativeStatsForTeam(team2_id))
 
-    def getCumulativeStatsForTeam(team_id: str) -> dict:
-        pass
+    def getCumulativeStatsForTeam(self, team_id: str) -> dict:
+        return self.team_stats[team_id]['cumulativeStats']
+    
+    def _getTeamIdsFromGameInfo(self, game_info: dict) -> tuple(str, str):
+        return ("team-1-id", "team-2-id")
+
+    # Update the cumulative stats for the given team_id
+    # after they have played a game that resulted in 'game_info' and 'stats_info'
+    def _updateTeamCumulativeStats(self, team_id: str, game_info: dict, stats_info: dict) -> dict:
+        # Add handling for if this is the first game this team has played
+        new_n_games: int = self.team_stats[team_id]['n_games'] + 1
+        old_weighted_count: Number = self.team_stats[team_id]['weighted_count']
+        new_weighted_count: Number = old_weighted_count + (0.9**new_n_games)
+        new_cumulative_stats = {}
+        for key, value in self.getCumulativeStatsForTeam(team_id=team_id).items():
+            # Check if this works for all keys. They might not all be numerical
+            new_cumulative_stats[key] = (value*old_weighted_count*0.9+value)/new_weighted_count
+        
+        self.team_stats[team_id]['n_games'] = new_n_games
+        self.team_stats[team_id]['weighted_count'] = new_weighted_count
+        self.team_stats[team_id]['cumulativeStats'] = new_cumulative_stats
+        return new_cumulative_stats
+        
