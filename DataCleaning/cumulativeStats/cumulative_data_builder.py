6d4b68865b30b36e8db038263dbf06f467f2c329
MatthewMyWu <matthew6086888@hotmail.com>
2023-10-02 13:03:44 -0600

    Add some documentation

--- DataCleaning/cumulativeStats/cumulative_data_builder.py
+++ DataCleaning/cumulativeStats/cumulative_data_builder.py
@@ -3,6 +3,19 @@
 class Cumulative_Stats_Builder:
     def __init__(self) -> Cumulative_Stats_Builder:
         self.team_stats = {}
+        """
+        Teams stats will look like this:
+        {
+            id: {
+                n_games: int,
+                weighted_count: number,
+                cumulativeStats: {
+                    ...
+                }
+            },
+            ...
+        }
+        """
         
     def addGamePlayed(game_info: dict, stats_info: dict) -> tuple(dict, dict):
         pass
