43814dd8a6f273a7798aab6d403b243769a4465d
dingdongg <cs.dg.kim@gmail.com>
2023-10-13 23:21:48 -0700

    update interface to include dataset split method

--- Models/ranking_model_interface.py
+++ Models/ranking_model_interface.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List
+from DataCleaning.datasetPrep.cumulative_df import get_training_and_test_datasets
 
 class Ranking_Model(ABC):
     """
@@ -23,3 +24,6 @@ class Ranking_Model(ABC):
     @abstractmethod
     def get_global_rankings(self, n_teams: int = 20) -> List[dict]:
         pass
+
+    def get_training_test_datasets(self, split_x_and_y = True, log_results = False) -> list:
+        return get_training_and_test_datasets(split_x_and_y=split_x_and_y, log_results=log_results)
