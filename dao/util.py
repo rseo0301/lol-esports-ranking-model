bdab7b05b7510c3a46c38b5a95b1c7a110ce206d
MatthewMyWu <matthew6086888@hotmail.com>
2023-10-16 13:55:43 -0600

    getCumulativeStatsForTeams implementation done

--- dao/util.py
+++ dao/util.py
@@ -5,7 +5,7 @@ current_script_directory = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_script_directory)
 from database_accessor import Database_Accessor
 import json
-from typing import Tuple
+from typing import Tuple, List
 
 # TODO Could probably build a cache for mapping table
 def getTeamIdsFromGameInfo(db_accessor: Database_Accessor, game_info: dict) -> Tuple[str, str]:
@@ -23,6 +23,31 @@ def getTeamIdsFromGameInfo(db_accessor: Database_Accessor, game_info: dict) -> T
 def getWinningTeam(game_info: dict) -> int:
     return game_info['game_end']['winningTeam']
 
+# Returns the latest cumulative stats for the list of teams
+def getCumulativeStatsForTeams(db_accessor: Database_Accessor, team_ids: List[str]) -> dict:
+    id_filter = [f"id='{id}'" for id in team_ids]
+    teams_data = db_accessor.getDataFromTable(tableName="teams", columns=["id", "latest_cumulative_stats"], where_clause=" OR ".join(id_filter))
+    ret = {}
+    for team_data in teams_data:
+        team_id = team_data[0]
+        cumulative_stats = json.loads(team_data[1])
+        ret[team_id] = cumulative_stats
+    return ret
+
+# Get the cumulative stats for every team, just before they play the first game of the tournmanet/stage
+def getCumulativeDataForTournament(tournament_id: str, stage: str) -> dict:
+    pass
+    """
+    {
+        team1_id: {cumulative_stats},
+        …
+    }
+    """
+
+
 # Debugging and testing:
 if __name__ == "__main__":
+    dao: Database_Accessor = Database_Accessor()
+    temp = getCumulativeStatsForTeams(db_accessor=dao, team_ids=["107580483738977500", "109981647134921596"])
+    print(temp["109981647134921596"])
     print("Hello World")
